8b544b2c2c34640b137ff529fb48ff46e7b7790e
김나윤 <umji@a1mobilsoft.com>
2026-09-15 18:03:17 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,20 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/2609_preferred/',
+
   plugins: [react(), tailwindcss()],
+
+  build: {
+    outDir: 'docs',
+  },
+
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
     },
   },
+
   server: {
     port: 5174,
   },
